cbeb3a7e32da125ed67257c5ae2e7102d53f450c
Chris McCuller <chris.mcculler@gmail.com>
2020-11-08 15:50:35 -0500

    :sparkles: disconnect clients after changing PW

--- index.js
+++ index.js
@@ -11,27 +11,52 @@ const config = {
   host: process.env.UNIFI_HOST_IP,
   port: process.env.UNIFI_HOST_PORT || 8443,
   site: process.env.UNIFI_SITE_NAME || 'default',
-  wlanId: process.env.UNIFI_WLAN_ID
+  wlan: process.env.UNIFI_WLAN_SSID,
+  clients: process.env.UNIFI_CLIENTS.split(',')
 }
 
 var controller = new unifi.Controller(config.host, config.port);
 
 // LOGIN
 controller.login(config.username, config.password, function(err) {
-  if(err) {
-    console.log('ERROR: ' + err);
-    controller.logout();
-    return;
-  }
-
-  const new_password = xkpasswd({ pattern: 'wsw' });
-  controller.setWLanSettings(config.site, config.wlanId, (err, res) => {
-    if (err) {
-      console.log(`Error: ${err}`);
+  controller.getWLanSettings(config.site, (err, res) => {
+    if(err) {
+      console.error(`ERROR: ${err}`);
+      controller.logout();
       return;
     }
 
-    console.log(`New password is ${new_password}`);
-  }, new_password);
-  controller.logout();
+    const wlan = res[0].find(wlan => wlan.name === config.wlan);
+    if (!wlan) {
+      console.error(`ERROR: Unable to find wlan with name '${config.wlan}'. Exiting`);
+      controller.logout();
+      return;
+    }
+    const wlanId = wlan._id
+
+    // set a new password for the specified network
+    const new_password = xkpasswd({ pattern: 'wsw' });
+    controller.setWLanSettings(config.site, wlanId, (err, res) => {
+      if (err) {
+        console.error(`ERROR: ${err}`);
+        controller.logout();
+        return;
+      }
+
+      console.log(`New password is ${new_password}`);
+
+      // Disconnect devices
+      let disconnectErrors = [];
+      config.clients.forEach(client => {
+        controller.reconnectClient(config.site, client, (err) => {
+          if (err) {
+            disconnectErrors.push(err);
+            return;
+          }
+        });
+      })
+
+      controller.logout();
+    }, new_password);
+  });
 });
